ecaf814acba24027ae7e620f11c51939a94c7a8c
Takayuki Maeda <yukinocist@nuinui.fml.org>
2024-06-14 00:07:32 +0900

    ChatGPT用にプロンプトをカスタマイズ

--- js/generate-prompt.js
+++ js/generate-prompt.js
@@ -1,27 +1,26 @@
 (async () => {
-  const ABCD = ['A', 'B', 'C', 'D'];
+	const ABCD = ["A", "B", "C", "D"];
 
-  let question = document.querySelector(
-    '.min-h-40.my-7.p-6.pb-8.bg-white.text-lg.border.border-light-blue-darkest'
-  );
-  let answers = document.querySelectorAll(
-    '.pointer-events-none.text-center.text-base.font-medium'
-  );
+	const question = document.querySelector(
+		".min-h-40.my-7.p-6.pb-8.bg-white.text-lg.border.border-light-blue-darkest",
+	);
+	const answers = document.querySelectorAll(
+		".pointer-events-none.text-center.text-base.font-medium",
+	);
 
-  let choices = ABCD.slice(0, answers.length).join(', ');
-  let prompt = `次の問題の回答は${choices}のうち、どれでしょうか。回答に対する解説となぜ他の回答が誤りなのかを含めて教えてください。\n`;
-  prompt += `また、与えた参考情報のPDFから回答について詳しく説明しているページも教えてください。\n\n`;
-  prompt += `問題文：\n${question.innerHTML}\n\n`;
-  for (let [index, answer] of Object.entries(answers)) {
-    prompt += ABCD[index] + '. ' + answer.innerHTML + '\n';
-  }
+	const choices = ABCD.slice(0, answers.length).join(", ");
+	let prompt = `次の問題の回答は${choices}のうち、どれでしょうか。回答に対する解説となぜ他の回答が誤りなのかを含めて教えてください。\n`;
+	prompt += `問題文：\n${question.innerHTML}\n\n`;
+	for (const [index, answer] of Object.entries(answers)) {
+		prompt += `${ABCD[index]}. ${answer.innerHTML}\n`;
+	}
 
-  navigator.clipboard.writeText(prompt).then(
-    () => {
-      console.log(`promptの作成に成功しました。`);
-    },
-    () => {
-      console.log('promptの作成に失敗しました。');
-    }
-  );
+	navigator.clipboard.writeText(prompt).then(
+		() => {
+			console.log("promptの作成に成功しました。");
+		},
+		() => {
+			console.log("promptの作成に失敗しました。");
+		},
+	);
 })();

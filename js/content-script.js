a3f925f00c4d3c8cc8117f129bd2a5f91b828c7b
Takayuki Maeda <yukinocist@nuinui.fml.org>
2024-06-14 00:44:51 +0900

    コード不備を修正

--- js/content-script.js
+++ js/content-script.js
@@ -1,45 +1,48 @@
 (async () => {
-  const loading = document.querySelectorAll(
-    '.flex.justify-center.items-center.h-full'
-  );
-  const options = {
-    threshold: 1,
-  };
-  const observer = new IntersectionObserver((entries) => {
-    for (const entry of entries) {
-      if (!entry.isIntersecting) {
-        setTimeout(() => {
-          const divs = document.querySelector('.px-6.pt-4.pb-6 ');
-          const label = document.querySelector(
-            '.flex.justify-between.tracking-wide.text-sm'
-          );
-          const answers = document.querySelector(
-            '.grid.grid-cols-1.md\\:grid-cols-2.gap-8.mt-5'
-          );
-          const arrayAnswers = Array.from(answers.children);
+	const loading = document.querySelector(
+		".flex.justify-center.items-center.h-full",
+	);
+	console.log(loading);
+	const options = {
+		threshold: 1,
+	};
+	const observer = new IntersectionObserver((entries) => {
+		console.log("hoge2");
+		for (const entry of entries) {
+			if (!entry.isIntersecting) {
+				setTimeout(() => {
+					const divs = document.querySelector(".px-6.pt-4.pb-6 ");
+					const label = document.querySelector(
+						".flex.justify-between.tracking-wide.text-sm",
+					);
+					const answers = document.querySelector(
+						".grid.grid-cols-1.md\\:grid-cols-2.gap-8.mt-5",
+					);
+					const arrayAnswers = Array.from(answers.children);
+					console.log(arrayAnswers);
 
-          answers.innerHTML = '';
-          divs.innerHTML = '';
+					answers.innerHTML = "";
+					divs.innerHTML = "";
 
-          const shuffledAnswers = shuffleArray(arrayAnswers);
-          for (const answer of shuffledAnswers) {
-            answers.appendChild(answer);
-          }
+					const shuffledAnswers = shuffleArray(arrayAnswers);
+					for (const answer of shuffledAnswers) {
+						answers.appendChild(answer);
+					}
 
-          divs.appendChild(label);
-          divs.appendChild(answers);
-        }, 100);
-      }
-    }
-  }, options);
+					divs.appendChild(label);
+					divs.appendChild(answers);
+				}, 200);
+			}
+		}
+	}, options);
 
-  observer.observe(loading);
+	observer.observe(loading);
 
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+	function shuffleArray(array) {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
 })();
